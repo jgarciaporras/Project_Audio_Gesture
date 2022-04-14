8ac0f6579b90069ffb400f127eaf1ac2662c82fa
jgarciaporras <joharichris.garciaporras@georgebrown.ca>
2022-04-14 18:41:43 -0400

    main

---
+++ main.py
@@ -0,0 +1,73 @@
+#app.py
+from flask import Flask, render_template, Response, request,flash,redirect,url_for
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
+from tasks import tasks
+
+from werkzeug.utils import secure_filename
+
+global capture,rec_frame, rec, out
+
+capture=0
+rec=0
+#TEMPLATE_DIR = os.path.abspath('../templates')
+#STATIC_DIR = os.path.abspath('../static')
+app = Flask(__name__, template_folder='templates')
+
+#camera = cv2.VideoCapture(0)
+
+
+ 
+UPLOAD_FOLDER = 'static/uploads/'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config["CACHE_TYPE"] = "null"
+
+ALLOWED_EXTENSIONS = set(['mp3','wav'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+ 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No audio selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Audio file successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - mp3/wav')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_audio(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+@app.route('/', methods=['POST'])
+def control_audio():
+    volume_20= tasks.volume_20()
+    return render_template('index.html', volume_20)
+ 
+
+if __name__ == "__main__":
+    app.run()
+#
---
+++ static/js/custom.js
@@ -0,0 +1,25 @@
+var audio= document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count=0;
+
+
+
+function playPause(){
+	if(count == 0){
+		count = 1;
+		audio.play();
+        playPauseBTN.innerHTML = "Pause &#9208;"
+    }else{
+        count = 0;
+		audio.pause();
+        playPauseBTN.innerHTML = "Play &#9658;"
+    }
+}
+
+function Stop(){
+    playPause()
+    audio.pause();
+    audio.currentTime= 0;
+    playPauseBTN.innerHTML = "Play &#9658;"
+
+}
